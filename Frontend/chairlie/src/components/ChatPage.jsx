14142e04744c3512b1c02bd3fdaef96fe51cfdd5
ElenaChanou <elenachanou87@gmail.com>
2024-07-14 02:58:21 +0300

    fix the import Login Page to ChatPAge.jsx

--- Frontend/chairlie/src/components/ChatPage.jsx
+++ Frontend/chairlie/src/components/ChatPage.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import './ChatPage.css';
-import './LoginPage';
+import LoginPage from "./LoginPage";
 
 function ChatPage() {
     const [userInput, setUserInput] = useState("");
