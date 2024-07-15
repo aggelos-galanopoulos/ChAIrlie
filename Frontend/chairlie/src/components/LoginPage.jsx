fe34110f7a278e8b86b9763686c739a0a0e02743
ElenaChanou <elenachanou87@gmail.com>
2024-07-15 12:17:55 +0300

    add loginLogo.svg to Login.jsx , create an images file inside src, I change the style of login page

--- Frontend/chairlie/src/components/LoginPage.jsx
+++ Frontend/chairlie/src/components/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import history from "../history";
-import "./LoginPage.css"
+import "./LoginPage.css";
+import logo from "../images/loginLogo.svg"; // Αντικατέστησε με το σωστό μονοπάτι της εικόνας σου
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -9,7 +10,6 @@ const LoginPage = () => {
   const handleLogin = () => {
     if (email && password) {
       console.log("Logging in with:", email, password);
-      // On successful login, navigate to the chat page
       history.push("/chat");
     } else {
       console.log("Email and Password are required");
@@ -17,21 +17,28 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h2>Login Page</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className="page-container">
+      <div className="left-side">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="right-side">
+        <div className="login-form">
+          <h2>Login Page</h2>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      </div>
     </div>
   );
 };
